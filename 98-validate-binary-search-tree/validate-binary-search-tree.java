0dc75af60f92f1e4c5c75b671c295b8255b188b8
Sushil Jaiswal <sjsushil09@gmail.com>
2025-05-30 02:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (60.15%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return validateBst(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+//     }
+
+//     private static boolean validateBst(TreeNode root,int low,int high) {
+//         if(root==null || (root.left == null && root.right == null)) 
+//             return true;
+
+//         return  (root.val>=low  && root.val<=high) &&
+//                 validateBst(root.left,low,root.val) &&
+//                 validateBst(root.right,root.val,high);
+//     }
+
+// }
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validateBst(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validateBst(TreeNode root, long low, long high) {
+        if (root == null) return true;
+
+        if (root.val <= low || root.val >= high) return false;
+
+        return validateBst(root.left, low, root.val) &&
+               validateBst(root.right, root.val, high);
+    }
+}
