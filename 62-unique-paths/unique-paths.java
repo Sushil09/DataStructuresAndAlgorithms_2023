e09cdd25f108cf4cdaf25f7d457ac61cb7d9bf64
Sushil Jaiswal <sjsushil09@gmail.com>
2026-05-17 16:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.70%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][]dp= new int[m][n];
+        for(int[]arr: dp)
+            Arrays.fill(arr,-1);
+        return countWays(m-1,n-1,dp);
+    }
+
+    private static int countWays(int m, int n, int[][]dp) {
+        if(m == 0 && n == 0) {
+            return 1;
+        }
+
+        if(m<0 || n<0)
+            return 0;
+
+        if(dp[m][n]!=-1)
+            return dp[m][n];    
+
+        return dp[m][n]= countWays(m-1,n,dp)+countWays(m,n-1,dp);
+    }
+}
