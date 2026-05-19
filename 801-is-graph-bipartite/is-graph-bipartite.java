e2facc7de194ecded0f5fa90236f07f22229fb54
Sushil Jaiswal <sjsushil09@gmail.com>
2026-05-19 13:45:13 +0530

    Time: 1 ms (61.92%) | Memory: 47 MB (76.27%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,36 +1,41 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-       int V = graph.length;
-       int[] vis= new int[V];
-       Arrays.fill(vis,-1);
-
-       for(int i=0;i<graph.length;i++){
-        if(vis[i]==-1) {
-            if(bfs(i,vis,graph) ==false)
-                return false;
-       }
-       }
-    return true;
-      
-    }
-
-    public boolean bfs(int src,int[]vis, int[][] graph){
-    Queue<Integer> q = new LinkedList<>();
-       vis[src]=0;
-       q.add(src);
-
-       while(!q.isEmpty()) {
-        int polled = q.poll();
-        for(int neighbor: graph[polled]) {
-            if(vis[neighbor]==-1) {
-                vis[neighbor] = 1-vis[polled];
-                q.add(neighbor);
-            }else{
-                if(1-vis[polled]!=vis[neighbor])
+        int N = graph.length;
+        int[]vis = new int[N];
+        Arrays.fill(vis,-1);
+        
+        for(int i=0;i<N;i++) {
+            if(vis[i]==-1){
+                if(bipartiteCheck(graph,vis,i) == false)
                     return false;
             }
         }
-       }
         return true;
     }
+
+    private static boolean bipartiteCheck(int[][] graph, int[]vis,int src) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+         vis[src]=0;
+        while(!queue.isEmpty()) {
+            int popped = queue.poll();
+            for(int neighbour : graph[popped]) {
+                if(vis[neighbour]==-1) {
+                    vis[neighbour] = 1-vis[popped];
+                    queue.add(neighbour);
+                }else{
+                    if(vis[neighbour]==vis[popped])
+                        return false;
+                }
+            }
+        }
+        return true;
+    } 
 }
+
+
+/*
+vis = [0,-1,-1,-1]
+
+
+*/
