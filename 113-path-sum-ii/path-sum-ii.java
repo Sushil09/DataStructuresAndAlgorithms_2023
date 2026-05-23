6cb96d589dbd88aeae6c87c9d971164ebf53925e
Sushil Jaiswal <sjsushil09@gmail.com>
2026-05-24 02:52:33 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (71.89%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -16,23 +16,24 @@
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> ans = new ArrayList<>();
-        findPathSum(root,ans, new ArrayList<>(),targetSum);
+        populateNodes(root,ans,new ArrayList<>(),targetSum);
         return ans;
     }
 
-    public void findPathSum(TreeNode root, List<List<Integer>> ans, List<Integer> temp, int K) {
-        if(root==null)
+    private static void populateNodes(TreeNode root, List<List<Integer>> ans, List<Integer> temp, int K) {
+        if(root==null) {
+           
             return;
-        
-        temp.add(root.val);
-        if(root.left==null && root.right == null && K == root.val) {
-                ans.add(new ArrayList<>(temp));
         }
-            else{
-            findPathSum(root.left,ans,temp,K-root.val);
-            findPathSum(root.right,ans,temp,K-root.val);
-            }
-     temp.remove(temp.size()-1);
-
+ temp.add(root.val);
+         if(root.left==null && root.right==null && root.val==K){
+            // temp.add(root.val);
+                ans.add(new ArrayList<>(temp));
+         }
+            // temp.add(root.val);
+            populateNodes(root.left,ans,temp,K-root.val);
+            populateNodes(root.right,ans,temp,K-root.val);
+            temp.remove(temp.size()-1);
+        
     }
 }
