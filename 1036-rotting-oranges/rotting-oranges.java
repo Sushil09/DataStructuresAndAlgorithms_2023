3dbe071e60301837ac13e2bf4d635e4636da5b11
Sushil Jaiswal <sjsushil09@gmail.com>
2026-05-28 00:32:55 +0530

    Time: 2 ms (90.63%) | Memory: 44.4 MB (24.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,49 +1,75 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int minutes = -1;
 
-        Queue<Pair> queue = new LinkedList<>();
-        for(int i=0;i<grid.length;i++) {
-            for(int j=0;j<grid[0].length;j++) {
-                if(grid[i][j]==2) {
-                    queue.add(new Pair(i,j));
+    public int orangesRotting(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        int fresh = 0;
+
+        // collect rotten oranges and count fresh oranges
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+
+                else if (grid[i][j] == 1) {
+                    fresh++;
                 }
             }
         }
 
-        int[]dx = {0,0,1,-1};
-        int[]dy = {1,-1,0,0};
+        // no fresh oranges already
+        if (fresh == 0)
+            return 0;
 
-        while(!queue.isEmpty()) {
+        int minutes = 0;
+
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        while (!queue.isEmpty()) {
+
             int size = queue.size();
-            minutes++;
-            for(int i=0;i<size;i++) {
-                Pair orange = queue.poll();
-                grid[orange.x][orange.y]=2;
-                for(int j=0;j<4;j++) {
-                    int newX = dx[j]+orange.x;
-                    int newY = dy[j]+orange.y;
-                    if(newX>=0 && newY>=0 && newX<grid.length && newY<grid[0].length && grid[newX][newY] ==1){
-                        grid[newX][newY]=2;
-                        queue.add(new Pair(newX,newY));
+
+            boolean rotted = false;
+
+            for (int i = 0; i < size; i++) {
+
+                int[] curr = queue.poll();
+
+                int x = curr[0];
+                int y = curr[1];
+
+                for (int d = 0; d < 4; d++) {
+
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+
+                    if (nx >= 0 && nx < n &&
+                        ny >= 0 && ny < m &&
+                        grid[nx][ny] == 1) {
+
+                        grid[nx][ny] = 2;
+
+                        fresh--;
+
+                        queue.offer(new int[]{nx, ny});
+
+                        rotted = true;
                     }
                 }
             }
+
+            if (rotted)
+                minutes++;
         }
-        for(int i=0;i<grid.length;i++) {
-            for(int j=0;j<grid[0].length;j++) {
-                if(grid[i][j]==1) {
-                    return -1;
-                }
-            }
-        }
-        return minutes==-1?0:minutes;
-    }
-    static class Pair{
-        int x,y;
-        Pair(int x, int y) {
-            this.x=x;
-            this.y=y;
-        }
+
+        return fresh == 0 ? minutes : -1;
     }
 }
