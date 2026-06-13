1126ca94068e71d07f2490f3a3edf38d6e1bb0a9
Sushil Jaiswal <sjsushil09@gmail.com>
2026-06-13 13:33:40 +0530

    Time: 15 ms (11.13%) | Memory: 50.1 MB (55.05%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,44 +1,39 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-       int totalWeight = sum(weights); 
-       int l = max(weights), h =totalWeight;
-       int minWeight = l;
-       while(l<=h) {
-        int mid = l+ (h-l)/2;
-            if(isPossible(weights,mid,days)){
-                minWeight = mid;
-                h = mid-1;
-            }else
-                l= mid+1;
-       }
-       return minWeight;
-    }
+        
+        int l = Arrays.stream(weights).max().getAsInt();
+        int h = Arrays.stream(weights).sum();
 
-    private boolean isPossible(int[]weights, int weight, int days) {
-        int tempWeight = 0, day =1;
-        for(int w: weights) {
-            
-            if(tempWeight+w>weight){
-                 tempWeight=0;
-                day++;
-            }    
-            tempWeight+=w;
+        int ans = h;
+
+        while(l<=h) {
+            int mid = l+(h-l)/2;
+
+            if(isPossible(weights,mid,days)) {
+                ans = mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+    }
+    return ans;
+  }
+
+  private static boolean isPossible(int[] weights,int mid, int days) {
+    int weight =0;
+    int day =1;
+    for(int i=0;i<weights.length;i++) {
+        weight+= weights[i];
+        if(weight>mid){
+            weight=weights[i];
+            day++;
         }
-
-         return day<=days;
     }
-
-    private int max(int[]weights) {
-        int ans = weights[0];
-        for(int x: weights)
-            ans = Math.max(x,ans);
-        return ans;    
-    }
-
-    private int sum(int[] weights) {
-        int ans = 0;
-        for(int x: weights) 
-            ans+=x;
-        return ans;
-    }
+    return day<=days;
+  }
 }
+
+
+/*
+O(range(high-low))
+*/
