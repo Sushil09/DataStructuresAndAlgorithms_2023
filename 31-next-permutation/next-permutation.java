824e9b405184f788cf83c8b356fefd1f02c54327
Sushil Jaiswal <sjsushil09@gmail.com>
2026-06-21 13:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (50.10%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,37 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-       int size = nums.length;
-       if(size == 1)
-        return ;
+        int idx=-1;
+        int n = nums.length;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i+1]>nums[i]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums,0,n-1);
+            return;
+        }
+            
 
-      int i=size-2;
-      while(i>=0 && nums[i] >= nums[i+1])
-        i--;    
-
-      if(i>=0){
-        int j=size-1;  
-      while(j>=0 && nums[i]>=nums[j]) 
-            j--;
-
-         swap(nums,i,j);    
-      }  
-       
-      reverse(nums,i+1);  
+        int nextEle = nums[n-1];
+        for(int i=n-1;i>idx;i--){
+            if(nums[i]>nums[idx]){
+                nextEle=i; 
+                break;
+            }
+        }
+        swap(nums,idx,nextEle);
+        reverse(nums,idx+1,n-1);    
     }
-    
-    private static void swap(int[]arr,int x,int y) {
-        int temp=arr[x];
-        arr[x]=arr[y];
-        arr[y]=temp;
+
+    private static void swap(int []arr,int i, int j) {
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 
-    private static void reverse(int[] arr, int idx) {
-        int i=idx,j=arr.length-1;
-
-        while(i<j) {
+    private static void reverse(int[]arr, int st, int end) {
+        int i=st,j=end;
+        while(i<=j) {
             swap(arr,i,j);
             i++;
             j--;
         }
     }
+
 }
