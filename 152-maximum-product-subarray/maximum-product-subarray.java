296c0ab939d354432e990d661d2bc28e199d1b96
Sushil Jaiswal <sjsushil09@gmail.com>
2026-06-22 21:12:22 +0530

    Time: 1 ms (92.83%) | Memory: 44.5 MB (99.94%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,30 +1,18 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
+        int ans =Integer.MIN_VALUE;
+        int prefix = 1;
+        int suffix = 1;
+        int size = nums.length;
+
+        for(int i=0;i<nums.length;i++) {
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+
+            prefix = prefix*nums[i];
+            suffix = suffix*nums[size-i-1];
+            ans = Math.max(ans,Math.max(prefix,suffix));
         }
-
-        int maxNegative = nums[0];
-        int maxPositive = nums[0];
-        int maxProduct = nums[0];   // Store the maximum result encountered
-
-        for (int i = 1; i < nums.length; i++) {
-            int temp = nums[i];
-
-            // Swap when negative to flip the min and max
-            if (temp < 0) {
-                int swap = maxPositive;
-                maxPositive = maxNegative;
-                maxNegative = swap;
-            }
-
-            maxPositive = Math.max(temp, maxPositive * temp);
-            maxNegative = Math.min(temp, maxNegative * temp);
-
-            // Update the max product encountered so far
-            maxProduct = Math.max(maxProduct, maxPositive);
-        }
-
-        return maxProduct;
+        return ans;
     }
 }
